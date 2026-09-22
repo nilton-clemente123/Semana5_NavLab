53420bdd25ad1b358d5c7e50d6e3b14f04988e5c
nilton-clemente123 <nilton.clemente@tecsup.edu.pe>
2026-09-21 22:57:36 -0500

    Agregar dependencia de iconos de Material Compose

--- app/src/main/java/com/clemente/navlab/Screens/DetailScreen.kt
+++ app/src/main/java/com/clemente/navlab/Screens/DetailScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
--- app/src/main/java/com/clemente/navlab/Screens/ListScreen.kt
+++ app/src/main/java/com/clemente/navlab/Screens/ListScreen.kt
@@ -2,6 +2,8 @@ package com.clemente.navlab.Screens
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
