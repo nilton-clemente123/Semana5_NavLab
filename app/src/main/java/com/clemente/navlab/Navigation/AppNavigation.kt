d488416f5188ef70d74d70bc4eda8cafa4699eba
nilton-clemente123 <nilton.clemente@tecsup.edu.pe>
2026-09-21 22:39:13 -0500

    Implementar rutas con Screen.kt y navegación de la aplicación con AppNavigation.kt

--- app/src/main/java/com/clemente/navlab/Navigation/AppNavigation.kt
+++ app/src/main/java/com/clemente/navlab/Navigation/AppNavigation.kt
@@ -1,7 +1,49 @@
 package com.clemente.navlab.Navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
+
+@Composable
+fun AppNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument(name = "itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) { backStackEntry ->
+
+            val itemId =
+                backStackEntry.arguments?.getInt("itemId") ?: 0
+
+            DetailScreen(navController, itemId)
+        }
+    }
+}
---
+++ app/src/main/java/com/clemente/navlab/Navigation/Screen.kt
@@ -0,0 +1,16 @@
+package com.clemente.navlab.Navigation
+sealed class Screen(val route: String) {
+
+
+    object Home : Screen(route = "home")
+
+
+    object List : Screen(route = "list")
+
+    object Profile : Screen(route = "profile")
+
+    object Detail : Screen(route = "detail/{itemId}") {
+
+        fun createRoute(itemId: Int): String = "detail/$itemId"
+    }
+}
